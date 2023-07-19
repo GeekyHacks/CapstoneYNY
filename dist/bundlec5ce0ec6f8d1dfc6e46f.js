a9ae0d4c29a7d8ee65f3bbb64705656f156c1692
GeekyHacks <geekyhacks22@gmail.com>
2023-07-19 23:56:55 +0300

    Finish header styling for mobile & desktop

--- dist/bundlec5ce0ec6f8d1dfc6e46f.js
+++ dist/bundlec5ce0ec6f8d1dfc6e46f.js
@@ -12,10 +12,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   headerSection: () => (/* binding */ headerSection)
 /* harmony export */ });
+/* harmony import */ var _assets_twitter_svg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../assets/twitter.svg */ "./src/assets/twitter.svg");
+/* harmony import */ var _assets_facebook_svg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../assets/facebook.svg */ "./src/assets/facebook.svg");
+/* harmony import */ var _assets_logo_svg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../assets/logo.svg */ "./src/assets/logo.svg");
+/* harmony import */ var _assets_mobile_menu_svg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../assets/mobile_menu.svg */ "./src/assets/mobile_menu.svg");
+
+
+
+
 var header = document.querySelector('header');
 var nav = document.createElement('nav');
 var headerSection = function headerSection() {
-  nav.innerHTML = " <nav>\n    <div id=\"fstHead\">\n      <ul>\n        <li><img src=\"images/facebook.svg\" alt=\"facebook\" /></li>\n        <li><img src=\"images/twitter.svg\" alt=\"twitter\" /></li>\n        <li><a href=\"#\">English</a></li>\n        <li><a href=\"index.html\">My Page</a></li>\n        <li><a href=\"#\">Logout</a></li>\n      </ul>\n    </div>\n    <div id=\"sndHead\">\n      <img id=\"menuBtn\" src=\"./assets/mobile_menu.svg\" alt=\"\" />\n      <img id=\"logo\" src=\"./assets/logo.svg\" alt=\"Party logo\" />\n      <ul id=\"labels\">\n        <li><a href=\"about.html\">About</a></li>\n        <li><a href=\".programs\">Program</a></li>\n        <li><a href=\"#\">Donate</a></li>\n        <li><a href=\"#\">Join</a></li>\n        <li><a href=\"#\">News</a></li>\n        <li><a id=\"bordered_label\" href=\"#fst_page\">YNY Campaign</a></li>\n      </ul>\n    </div>\n    </nav>";
+  nav.innerHTML = " <nav>\n    <div class=\"fstHead\">\n      <ul id=\"fstHead\">\n        <li><img src=\"".concat(_assets_facebook_svg__WEBPACK_IMPORTED_MODULE_1__, "\" alt=\"facebook\" /></li>\n        <li><img src=\"").concat(_assets_twitter_svg__WEBPACK_IMPORTED_MODULE_0__, "\" alt=\"twitter\" /></li>\n        <li><a href=\"#\">English</a></li>\n        <li><a href=\"index.html\">My Page</a></li>\n        <li><a href=\"#\">Logout</a></li>\n      </ul>\n    </div>\n    <div id=\"sndHead\">\n      <img id=\"menuBtn\" src=\"").concat(_assets_mobile_menu_svg__WEBPACK_IMPORTED_MODULE_3__, "\" alt=\"\" />\n      <img id=\"logo\" src=\"").concat(_assets_logo_svg__WEBPACK_IMPORTED_MODULE_2__, "\" alt=\"Party logo\" />\n      <ul id=\"labels\">\n        <li><a href=\"about.html\">About</a></li>\n        <li><a href=\".programs\">Program</a></li>\n        <li><a href=\"#\">Donate</a></li>\n        <li><a href=\"#\">Join</a></li>\n        <li><a href=\"#\">News</a></li>\n        <li><a id=\"borderLabel\" href=\"#fst_page\">YNY Campaign</a></li>\n      </ul>\n    </div>\n    </nav>");
   header.appendChild(nav);
 };
 
@@ -45,57 +53,117 @@ ___CSS_LOADER_EXPORT___.push([module.id, `* {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
-}
-
-* {
-  box-sizing: border-box;
+  list-style: none;
+  outline: none;
+  text-decoration: none;
 }
 
 body {
-  background-color: #2fa8cc;
+  background-color: transparent;
   font-family: "Roboto", sans-serif;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
+  font-size: 14px;
   overflow: hidden;
   margin: 0;
-  padding: 20px;
+  background: #dce3e6;
 }
 
-.container {
-  background-color: #f4f4f4;
-  border-radius: 10px;
+a {
+  color: #fbfaf8;
+}
+
+nav {
+  display: flex;
+  flex-direction: column;
+}
+
+.fstHead {
+  display: none;
+}
+
+#fstHead {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.3rem;
+  gap: 1rem;
+}
+
+#sndHead {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.3rem;
+  gap: 0.2rem;
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1);
-  padding: 50px 20px;
-  text-align: center;
-  max-width: 100%;
-  width: 800px;
+  background: #a7b7bd;
 }
 
-h3 {
-  margin: 0;
-  opacity: 0.5;
-  letter-spacing: 2px;
+#menuBtn {
+  flex: 0.2;
 }
 
-img {
-  width: 100px;
-  margin-bottom: 20px;
+#logo {
+  flex: 2;
+  height: 4rem;
 }
 
-.joke {
-  font-size: 30px;
-  letter-spacing: 1px;
-  line-height: 40px;
-  margin: 50px auto;
-  max-width: 600px;
+#labels {
+  display: none;
 }
 
+@media (min-width: 768px) {
+  nav {
+    gap: 0;
+  }
+  .fstHead {
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    background: #434c5e;
+  }
+  #fstHead, #sndHead {
+    padding-right: calc(20% - 100px);
+  }
+  #fstHead {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    padding-right: calc(25% - 120px);
+  }
+  #sndHead {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1);
+    background: #fbfaf8;
+    gap: 2rem;
+  }
+  #sndHead a {
+    color: #495057;
+  }
+  #menuBtn {
+    display: none;
+  }
+  #logo {
+    flex: 2;
+    height: 3rem;
+  }
+  #labels {
+    flex: 6;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    font-size: 0.8rem;
+  }
+}
 .btn {
-  background-color: #2fa8cc;
-  color: #f4f4f4;
+  background-color: #dce3e6;
+  color: #a7b7bd;
   border: 0;
   border-radius: 10px;
   box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1);
@@ -108,7 +176,7 @@ img {
 }
 .btn:focus {
   outline: 0;
-}`, "",{"version":3,"sources":["webpack://./src/styles/global.sass","webpack://./src/styles/main.sass"],"names":[],"mappings":"AAKA;EACI,sBAAA;EACA,SAAA;EACA,UAAA;ACHJ;;AACA;EACI,sBAAA;AAEJ;;AAAA;EACI,yBARY;EASZ,iCAAA;EACA,aAAA;EACA,sBAAA;EACA,mBAAA;EACA,uBAAA;EACA,aAAA;EACA,gBAAA;EACA,SAAA;EACA,aAAA;AAGJ;;AADA;EACI,yBAnBc;EAoBd,mBAAA;EACA,wEApBS;EAqBT,kBAAA;EACA,kBAAA;EACA,eAAA;EACA,YAAA;AAIJ;;AAFA;EACI,SAAA;EACA,YAAA;EACA,mBAAA;AAKJ;;AAHA;EACI,YAAA;EACA,mBAAA;AAMJ;;AAJA;EACI,eAAA;EACA,mBAAA;EACA,iBAAA;EACA,iBAAA;EACA,gBAAA;AAOJ;;AALA;EACI,yBA7CY;EA8CZ,cA7Cc;EA8Cd,SAAA;EACA,mBAAA;EACA,uEAAA;EACA,kBAAA;EACA,eAAA;EACA,eAAA;AAQJ;AANI;EACI,sBAAA;AAQR;AANI;EACI,UAAA;AAQR","sourcesContent":["$InterFont: \"Inter\", sans-serif\r\n$primary-color: #2fa8cc\r\n$secondary-color: #acc6e4\r\n$inputColor:#a8ccf5\r\n$box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1)\r\n*\r\n    box-sizing: border-box\r\n    margin: 0\r\n    padding: 0\r\n\r\n// mixings\r\n\r\n@mixin columnFlex\r\n    display: flex\r\n    flex-direction: column\r\n@mixin rowFlex\r\n    display: flex\r\n    flex-direction: row\r\n@mixin button\r\n    transition: all 0.5s\r\n    border-radius: 2px\r\n    font-family: $InterFont\r\n    font-weight: 500\r\n    font-size: 1rem\r\n    letter-spacing: 0.001em\r\n    word-spacing: normal\r\n    background-color: $primary-color\r\n    color: $secondary-color\r\n    border: 0\r\n    box-shadow: $box-shadow\r\n    padding: 10px\r\n    cursor: pointer\r\n    text-align: center\r\n\r\n@mixin twoThreeGrid\r\n    display: grid\r\n    grid-template-columns: 1fr 1fr\r\n    grid-template-rows: 1fr 1fr 1fr\r\n\r\n@mixin globalFont\r\n    font-family: $InterFont\r\n    font-weight: 500\r\n    font-size: 1.2rem\r\n@mixin smlInterH1\r\n    color: black\r\n    font-size: 2rem\r\n    font-family: $InterFont\r\n    font-weight: 800\r\n    letter-spacing: -0.0525rem\r\n@mixin smlInterH2\r\n    color: black\r\n    font-size: 1.5rem\r\n    font-family: $InterFont\r\n    font-weight: 800\r\n    letter-spacing: -0.0225rem\r\n@mixin smlInterH3\r\n    color: black\r\n    font-size: 1.2rem\r\n    font-family: $InterFont\r\n    font-weight: 600\r\n    letter-spacing: 0.0025rem\r\n@mixin smlInterP\r\n    color: black\r\n    font-size: .9rem\r\n    font-family: $InterFont\r\n    letter-spacing: 0.0125rem","@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap')\r\n@import global\r\n$primary-color: #2fa8cc\r\n$secondary-color: #f4f4f4\r\n$box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1)\r\n\r\n*\r\n    box-sizing: border-box\r\n\r\nbody\r\n    background-color: $primary-color\r\n    font-family: 'Roboto', sans-serif\r\n    display: flex\r\n    flex-direction: column\r\n    align-items: center\r\n    justify-content: center\r\n    height: 100vh\r\n    overflow: hidden\r\n    margin: 0\r\n    padding: 20px\r\n\r\n.container\r\n    background-color: $secondary-color\r\n    border-radius: 10px\r\n    box-shadow: $box-shadow\r\n    padding: 50px 20px\r\n    text-align: center\r\n    max-width: 100%\r\n    width: 800px\r\n\r\nh3\r\n    margin: 0\r\n    opacity: 0.5\r\n    letter-spacing: 2px\r\n\r\nimg\r\n    width: 100px\r\n    margin-bottom: 20px\r\n\r\n.joke\r\n    font-size: 30px\r\n    letter-spacing: 1px\r\n    line-height: 40px\r\n    margin: 50px auto\r\n    max-width: 600px\r\n\r\n.btn\r\n    background-color: $primary-color\r\n    color: $secondary-color\r\n    border: 0\r\n    border-radius: 10px\r\n    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1)\r\n    padding: 14px 40px\r\n    font-size: 16px\r\n    cursor: pointer\r\n\r\n    &:active\r\n        transform: scale(0.98)\r\n\r\n    &:focus\r\n        outline: 0\r\n"],"sourceRoot":""}]);
+}`, "",{"version":3,"sources":["webpack://./src/styles/global.sass","webpack://./src/styles/main.sass"],"names":[],"mappings":"AAQA;EACI,sBAAA;EACA,SAAA;EACA,UAAA;EACA,gBAAA;EACA,aAAA;EACA,qBAAA;ACNJ;;AALA;EACI,6BAAA;EACA,iCAAA;EACA,eAAA;EACA,gBAAA;EACA,SAAA;EACA,mBDRY;ACgBhB;;AAPA;EACI,cAAA;AAUJ;;AARA;EDMI,aAAA;EACA,sBAAA;ACMJ;;AAXA;EACI,aAAA;AAcJ;;AAbA;EDKI,aAAA;EACA,mBAAA;ECJA,8BAAA;EACA,mBAAA;EACA,eAAA;EACA,SAAA;AAiBJ;;AAfA;EDFI,aAAA;EACA,mBAAA;ECGA,8BAAA;EACA,mBAAA;EACA,eAAA;EACA,WAAA;EACA,wED3BS;EC4BT,mBD7Bc;ACgDlB;;AAjBA;EACI,SAAA;AAoBJ;;AAlBA;EACI,OAAA;EACA,YAAA;AAqBJ;;AAnBA;EACI,aAAA;AAsBJ;;AAnBA;EAEI;IACI,MAAA;EAqBN;EApBE;ID1BA,aAAA;IACA,mBAAA;IC2BI,oBAAA;IACA,mBAAA;EAuBN;EApBE;IACI,gCDnDI;ECyEV;EApBE;IDnCA,aAAA;IACA,mBAAA;ICoCI,6BAAA;IACA,mBAAA;IACA,gCDxDI;EC+EV;EArBE;IDzCA,aAAA;IACA,mBAAA;IC0CI,8BAAA;IACA,mBAAA;IACA,wEDhEK;ICiEL,mBAAA;IACA,SAAA;EAwBN;EAtBM;IACI,cAAA;EAwBV;EAtBE;IACI,aAAA;EAwBN;EAtBE;IACI,OAAA;IACA,YAAA;EAwBN;EAtBE;IACI,OAAA;ID5DJ,aAAA;IACA,mBAAA;IC6DI,6BAAA;IACA,mBAAA;IAEA,iBAAA;EAwBN;AACF;AAtBA;EACI,yBD1FY;EC2FZ,cD1Fc;EC2Fd,SAAA;EACA,mBAAA;EACA,uEAAA;EACA,kBAAA;EACA,eAAA;EACA,eAAA;AAwBJ;AAtBI;EACI,sBAAA;AAwBR;AAtBI;EACI,UAAA;AAwBR","sourcesContent":["$InterFont: \"Inter\", sans-serif\r\n$primary-color: #dce3e6\r\n$secondary-color: #a7b7bd\r\n$box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1)\r\n$qrpadding: calc( 20% - 100px )\r\n$Qrpadding: calc( 25% - 120px )\r\n$qlpadding: calc( 20% - 100px )\r\n$Qlpadding: calc( 25% - 120px )\r\n*\r\n    box-sizing: border-box\r\n    margin: 0\r\n    padding: 0\r\n    list-style: none\r\n    outline: none\r\n    text-decoration: none\r\n\r\n// mixings\r\n\r\n@mixin columnFlex\r\n    display: flex\r\n    flex-direction: column\r\n@mixin rowFlex\r\n    display: flex\r\n    flex-direction: row\r\n@mixin button\r\n    transition: all 0.5s\r\n    border-radius: 2px\r\n    font-family: $InterFont\r\n    font-weight: 500\r\n    font-size: 1rem\r\n    letter-spacing: 0.001em\r\n    word-spacing: normal\r\n    background-color: $primary-color\r\n    color: $secondary-color\r\n    border: 0\r\n    box-shadow: $box-shadow\r\n    padding: 10px\r\n    cursor: pointer\r\n    text-align: center\r\n\r\n@mixin twoThreeGrid\r\n    display: grid\r\n    grid-template-columns: 1fr 1fr\r\n    grid-template-rows: 1fr 1fr 1fr\r\n\r\n@mixin globalFont\r\n    font-family: $InterFont\r\n    font-weight: 500\r\n    font-size: 1.2rem\r\n@mixin smlInterH1\r\n    color: black\r\n    font-size: 2rem\r\n    font-family: $InterFont\r\n    font-weight: 800\r\n    letter-spacing: -0.0525rem\r\n@mixin smlInterH2\r\n    color: black\r\n    font-size: 1.5rem\r\n    font-family: $InterFont\r\n    font-weight: 800\r\n    letter-spacing: -0.0225rem\r\n@mixin smlInterH3\r\n    color: black\r\n    font-size: 1.2rem\r\n    font-family: $InterFont\r\n    font-weight: 600\r\n    letter-spacing: 0.0025rem\r\n@mixin smlInterP\r\n    color: black\r\n    font-size: .9rem\r\n    font-family: $InterFont\r\n    letter-spacing: 0.0125rem\r\n","@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap')\r\n@import global\r\n\r\nbody\r\n    background-color: transparent\r\n    font-family: 'Roboto', sans-serif\r\n    font-size: 14px\r\n    overflow: hidden\r\n    margin: 0\r\n    background: $primary-color\r\na\r\n    color: #fbfaf8\r\n// nav section mobile section\r\nnav\r\n    @include columnFlex\r\n.fstHead\r\n    display: none\r\n#fstHead\r\n    @include rowFlex\r\n    justify-content: space-between\r\n    align-items: center\r\n    padding: .3rem\r\n    gap: 1rem\r\n\r\n#sndHead\r\n    @include rowFlex\r\n    justify-content: space-between\r\n    align-items: center\r\n    padding: .3rem\r\n    gap: .2rem\r\n    box-shadow: $box-shadow\r\n    background: $secondary-color\r\n\r\n#menuBtn\r\n    flex: .2\r\n\r\n#logo\r\n    flex: 2\r\n    height: 4rem\r\n\r\n#labels\r\n    display: none\r\n\r\n// nav section desktop section\r\n@media ( min-width: 768px  )\r\n\r\n    nav\r\n        gap: 0\r\n    .fstHead\r\n        @include rowFlex\r\n        justify-content: end\r\n        background: #434c5e\r\n\r\n    // padding elements \r\n    #fstHead, #sndHead\r\n        padding-right: $qrpadding\r\n\r\n    #fstHead\r\n        @include rowFlex\r\n        justify-content: space-evenly\r\n        align-items: center\r\n        padding-right: $Qrpadding\r\n\r\n    #sndHead\r\n        @include rowFlex\r\n        justify-content: space-between\r\n        align-items: center\r\n        box-shadow: $box-shadow\r\n        background: #fbfaf8\r\n        gap: 2rem\r\n\r\n        a\r\n            color: #495057\r\n\r\n    #menuBtn\r\n        display: none\r\n\r\n    #logo\r\n        flex: 2\r\n        height: 3rem\r\n\r\n    #labels\r\n        flex: 6\r\n        @include rowFlex\r\n        justify-content: space-evenly\r\n        align-items: center\r\n        // gap: 1rem\r\n        font-size: .8rem\r\n        \r\n\r\n.btn\r\n    background-color: $primary-color\r\n    color: $secondary-color\r\n    border: 0\r\n    border-radius: 10px\r\n    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1)\r\n    padding: 14px 40px\r\n    font-size: 16px\r\n    cursor: pointer\r\n\r\n    &:active\r\n        transform: scale(0.98)\r\n\r\n    &:focus\r\n        outline: 0\r\n"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -553,6 +621,46 @@ function styleTagTransform(css, styleElement) {
 }
 module.exports = styleTagTransform;
 
+/***/ }),
+
+/***/ "./src/assets/facebook.svg":
+/*!*********************************!*\
+  !*** ./src/assets/facebook.svg ***!
+  \*********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "facebook.svg";
+
+/***/ }),
+
+/***/ "./src/assets/logo.svg":
+/*!*****************************!*\
+  !*** ./src/assets/logo.svg ***!
+  \*****************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "logo.svg";
+
+/***/ }),
+
+/***/ "./src/assets/mobile_menu.svg":
+/*!************************************!*\
+  !*** ./src/assets/mobile_menu.svg ***!
+  \************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "mobile_menu.svg";
+
+/***/ }),
+
+/***/ "./src/assets/twitter.svg":
+/*!********************************!*\
+  !*** ./src/assets/twitter.svg ***!
+  \********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "twitter.svg";
+
 /***/ })
 
 /******/ 	});
@@ -606,6 +714,18 @@ module.exports = styleTagTransform;
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -620,6 +740,29 @@ module.exports = styleTagTransform;
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/nonce */
@@ -646,4 +789,4 @@ window.onload = (0,_modules_header_js__WEBPACK_IMPORTED_MODULE_1__.headerSection
 
 /******/ })()
 ;
-//# sourceMappingURL=bundled0c76ee3ab651f3fa9c1.js.map
+//# sourceMappingURL=bundlec5ce0ec6f8d1dfc6e46f.js.map
